f9a1ae63ffdda3043f73c6bbb12fd4e8119c5133
Marcelo Romero <marcelorom@hotmail.com>
2026-03-24 10:21:40 +0100

    add Auth fixture

---
+++ tests/fixtures/auth.fixture.js
@@ -0,0 +1,38 @@
+import { test as base } from '@playwright/test';
+import { getOtpfromAPP } from '../helpers/index.js';
+import { LoginPage, Verify2FAPage } from '../pageObjectModel/pages/index.js';
+import { HeaderSection } from '../pageObjectModel/sections/index.js';
+import { loginUser } from '../test-data/users.js';
+
+export const test = base.extend({
+  pageObjects: async ({ page }, use) => {
+    const pageObjects = {
+      page,
+      loginPage: new LoginPage(page),
+      verify2FAPage: new Verify2FAPage(page),
+      headerSection: new HeaderSection(page)
+    };
+    
+    await use(pageObjects);
+  },
+
+  performLogin: async ({ pageObjects }, use) => {
+    const loginFunction = async (userType) => {
+      const user = loginUser[userType];
+      let otp;
+
+      await pageObjects.loginPage.goto();
+      await pageObjects.loginPage.enterUserCredentials(user.email, user.password);
+      await pageObjects.loginPage.clickLoginButton();
+
+      otp = getOtpfromAPP(user);
+      await pageObjects.verify2FAPage.enterOTP(otp);
+
+      await pageObjects.headerSection.userGreeting().waitFor({ state: 'visible' });
+    };
+
+    await use(loginFunction);
+  }
+});
+
+export { expect } from '@playwright/test';
