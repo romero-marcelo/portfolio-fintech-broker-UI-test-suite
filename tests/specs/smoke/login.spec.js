f914b37bee3cc8f5ae296f05133a6dc6a9e0bcd3
Marcelo Romero <marcelorom@hotmail.com>
2026-03-24 11:50:02 +0100

    fix login spec to call userType correctly

--- tests/specs/smoke/login.spec.js
+++ tests/specs/smoke/login.spec.js
@@ -4,18 +4,18 @@ import { loginUser } from '../../test-data/users.js';
 test.describe('Login Tests', () => {
 
   // REGULAR USER
-  test('Regular User Login, using password and 2FA-APP', async ({ pageObjects, performLogin }) => {
+  test('Regular User Login, using password and 2FA-APP', async ({ pageObjects, performLogin, page }) => {
     
-    await performLogin('regular');
+    await performLogin(loginUser.regular);
 
     await expect(pageObjects.headerSection.userGreeting()).toHaveText(`Hey, ${loginUser.regular.name}`);
-    await expect(pageObjects.page).toHaveURL(/.*dashboard.*/);
+    await expect(page).toHaveURL(/.*dashboard.*/);
   });
 
   // CORPORATE USER           
-  test('Corporate User Login, using password and 2FA-APP', async ({ pageObjects, performLogin }) => {
+  test('Corporate User Login, using password and 2FA-APP', async ({ pageObjects, performLogin, page }) => {
     
-    await performLogin('corporate');
+    await performLogin(loginUser.corporate);
 
     // Assert
     await expect(pageObjects.headerSection.userGreeting()).toHaveText(`Hey, ${loginUser.corporate.name}`);
